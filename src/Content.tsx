6ec7416b5272b5fba1f44e5fbe92b9dd86c9f8d8
philly <philly@phillyui-MacBookAir.local>
2022-12-21 16:03:39 +0900

    feat: content script에 selectionchange 이벤트리스너 추가

--- src/Content.tsx
+++ src/Content.tsx
@@ -1,4 +1,23 @@
+import { useEffect } from "react";
+
 function Content() {
+  useEffect(() => {
+    document.addEventListener("selectionchange", async (event) => {
+      const selection = window.getSelection();
+      if (selection === null) return;
+
+      const selectedText = selection.toString();
+      if (selectedText.length === 0) return;
+
+      chrome.runtime.sendMessage(
+        { type: "selectedText", data: selectedText },
+        (response) => {
+          // console.log(response);
+        }
+      );
+    });
+  }, []);
+
   return <div>This is Content component</div>;
 }
 
