b61ba819e7ccc3e030326ab9f6fe81a8a0ddb369
philly <philly@phillyui-MacBookAir.local>
2022-12-21 23:25:50 +0900

    feat: 번역된 영어 툴팁으로 보여주는 기능 구현

--- src/Content.tsx
+++ src/Content.tsx
@@ -1,24 +1,104 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 function Content() {
+  const [translatedText, setTranslatedText] = useState("");
+  const [rectPosition, setRectPosition] = useState<{
+    top: number;
+    left: number;
+  }>({ top: 0, left: 0 });
+  const [isHotKeyPressed, setIsHotKeyPressed] = useState(false);
+
   useEffect(() => {
-    document.addEventListener("selectionchange", async (event) => {
+    const handleSelectionChange = () => {
       const selection = window.getSelection();
       if (selection === null) return;
+      if (selection.isCollapsed) {
+        setTranslatedText("");
+        setRectPosition({ top: 0, left: 0 });
+        return;
+      }
+
+      const range = selection.getRangeAt(0);
+      const rect = range.getBoundingClientRect();
+      setRectPosition({
+        top: rect.y + rect.height * 1.2 + window.scrollY,
+        left: rect.x,
+      });
 
       const selectedText = selection.toString();
       if (selectedText.length === 0) return;
+      const isSupportedText = (text: string) => /^[A-Za-z\s\_]*$/.test(text);
+      if (!isSupportedText(selectedText)) return;
 
       chrome.runtime.sendMessage(
         { type: "selectedText", data: selectedText },
         (response) => {
-          // console.log(response);
+          setTranslatedText(response.data);
         }
       );
-    });
+    };
+
+    document.addEventListener("selectionchange", handleSelectionChange);
+    return () => {
+      document.removeEventListener("selectionchange", handleSelectionChange);
+    };
   }, []);
 
-  return <div>This is Content component</div>;
+  useEffect(() => {
+    const IS_MAC_OS = /Mac OS X/.test(navigator.userAgent);
+    const getHotKey = (event: KeyboardEvent) =>
+      IS_MAC_OS ? event.metaKey : event.ctrlKey;
+    const isHotKey = (event: KeyboardEvent) =>
+      event.key === "Meta" || event.key === "Control";
+
+    const handleKeyDown = (event: KeyboardEvent) => {
+      if (event.repeat) return;
+      if (!isHotKey) return;
+
+      const hotKey = getHotKey(event);
+      setIsHotKeyPressed(hotKey);
+    };
+    const handleKeyUp = (event: KeyboardEvent) => {
+      if (!isHotKey) return;
+
+      const hotKey = getHotKey(event);
+      setIsHotKeyPressed(hotKey);
+    };
+
+    document.addEventListener("keydown", handleKeyDown);
+    document.addEventListener("keyup", handleKeyUp);
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown);
+      document.removeEventListener("keyup", handleKeyUp);
+    };
+  }, []);
+
+  return (
+    <div>
+      {translatedText && isHotKeyPressed && (
+        <div
+          style={{
+            width: "max-content",
+            maxWidth: "500px",
+            position: "absolute",
+            top: rectPosition.top,
+            left: rectPosition.left,
+            zIndex: 2147483647,
+            background: "rgb(0,0,0)",
+            color: "rgb(255,255,255)",
+            padding: "8px",
+            fontSize: "16px",
+            lineHeight: 1.5,
+            borderColor: "rgba(255, 255, 255, 0.01)",
+            borderRadius: "10px",
+          }}
+          dangerouslySetInnerHTML={{
+            __html: translatedText.replace(/([2-9]\.)/g, "<br/>$1"),
+          }}
+        />
+      )}
+    </div>
+  );
 }
 
 export default Content;
