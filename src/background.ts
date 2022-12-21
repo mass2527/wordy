a97e208f7adc1ce14b70da606b5227ea98c03788
philly <philly@phillyui-MacBookAir.local>
2022-12-21 17:48:03 +0900

    feat: background service_worker 영단어 크롤링 기능 구현

--- src/background.ts
+++ src/background.ts
@@ -1,6 +1,18 @@
+import cheerio from "cheerio";
+
 // Listen for messages from the content script
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  console.info({ message }, "from background.ts");
+  fetch(`https://dic.daum.net/search.do?q=${message.data}`)
+    .then((response) => response.text())
+    .then((htmlString) => {
+      const $ = cheerio.load(htmlString);
+      const searchBox = $(".search_box").first();
+      const searchListItems = searchBox.find(".list_search").find("li");
+      const searchResult = searchListItems.text();
+      sendResponse({ data: searchResult });
+    });
+
+  return true;
 });
 
 export default {};
