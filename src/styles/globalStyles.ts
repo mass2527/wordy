ff777dd9c75f9aede15dbf9374f2a10520630dfc
Dongho Kim <dongre2527@gmail.com>
2022-12-28 18:31:16 +0900

    feat: globalStyles 변경

--- src/styles/globalStyles.ts
+++ src/styles/globalStyles.ts
@@ -1,13 +1,14 @@
 import { globalCss } from './stitches.config';
 
 export const globalStyles = globalCss({
+  '*': { margin: 0, padding: 0 },
   '*, *::before, *::after': { boxSizing: 'border-box' },
   html: {},
   body: {
-    margin: 0,
     color: '$neutral800',
     fontFamily: '$untitled',
     fontSize: '$16',
     '-webkit-font-smoothing': 'antialiased',
+    lineHeight: '1.5',
   },
 });
