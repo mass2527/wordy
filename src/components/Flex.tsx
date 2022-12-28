0428c4f085bb49e7ec9a0da8e0eb4f5ce3139ce5
Dongho Kim <dongre2527@gmail.com>
2022-12-28 17:29:43 +0900

    feat: Flex 컴포넌트 추가

---
+++ src/components/Flex.tsx
@@ -0,0 +1,82 @@
+import { styled } from '../styles/stitches.config';
+import { Box } from './Box';
+
+export const Flex = styled(Box, {
+  display: 'flex',
+
+  variants: {
+    direction: {
+      row: {
+        flexDirection: 'row',
+      },
+      column: {
+        flexDirection: 'column',
+      },
+      rowReverse: {
+        flexDirection: 'row-reverse',
+      },
+      columnReverse: {
+        flexDirection: 'column-reverse',
+      },
+    },
+    align: {
+      start: {
+        alignItems: 'flex-start',
+      },
+      center: {
+        alignItems: 'center',
+      },
+      end: {
+        alignItems: 'flex-end',
+      },
+      stretch: {
+        alignItems: 'stretch',
+      },
+      baseline: {
+        alignItems: 'baseline',
+      },
+    },
+    justify: {
+      start: {
+        justifyContent: 'flex-start',
+      },
+      center: {
+        justifyContent: 'center',
+      },
+      end: {
+        justifyContent: 'flex-end',
+      },
+      between: {
+        justifyContent: 'space-between',
+      },
+    },
+    wrap: {
+      noWrap: {
+        flexWrap: 'nowrap',
+      },
+      wrap: {
+        flexWrap: 'wrap',
+      },
+      wrapReverse: {
+        flexWrap: 'wrap-reverse',
+      },
+    },
+    gap: {
+      4: {
+        gap: '$4',
+      },
+      8: {
+        gap: '$8',
+      },
+      16: {
+        gap: '$16',
+      },
+    },
+  },
+  defaultVariants: {
+    direction: 'row',
+    align: 'stretch',
+    justify: 'start',
+    wrap: 'noWrap',
+  },
+});
