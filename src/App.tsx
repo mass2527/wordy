e9121a1445170c107bc6ce2b74fca777ec61488d
Dongho Kim <dongre2527@gmail.com>
2022-12-28 20:45:31 +0900

    feat: Popup 마크업

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,39 @@
+import { Box } from './components/Box';
+import { Flex } from './components/Flex';
+import { Switch } from './components/Switch';
+import { Text } from './components/Text';
+
 function App() {
   return (
-    <div>
-      <div>
+    <Box css={{ width: '22rem' }}>
+      <Flex
+        as='header'
+        gap={8}
+        align='center'
+        css={{ p: '$16', borderBottom: '1px solid $neutral200' }}
+      >
         <img src="icon16.png" alt="wordy logo" width={24} height={24} />
-        <span>wordy</span>
-      </div>
-    </div>
+        <Text as='h1'>
+          wordy{' '}
+          <Text color='primary' css={{ fontWeight: '$700' }}>
+            for Chrome
+          </Text>
+        </Text>
+      </Flex>
+      <Flex as='main' direction='column' gap={8} css={{ p: '$16' }}>
+        <Text as='h2' css={{ fontWeight: '$700' }}>
+          설정
+        </Text>
+        <Flex justify='between' align='center'>
+          <Text size={14}>마우스를 단어에 올리면 뜻 확인</Text>
+          <Switch />
+        </Flex>
+        <Flex justify='between' align='center'>
+          <Text size={14}>발음기호 및 발음 정보 표시</Text>
+          <Switch />
+        </Flex>
+      </Flex>
+    </Box>
   );
 }
 
