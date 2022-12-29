77e1ead671a0bb7cbf8f32fabf87de5f9ad889e3
Dongho Kim <dongre2527@gmail.com>
2022-12-29 17:11:42 +0900

    feat: useChromeStorageState hook 추가

---
+++ src/hooks/useChromeStorageState.ts
@@ -0,0 +1,52 @@
+import { useCallback, useEffect, useState } from 'react';
+
+type ChromeStorage = typeof chrome.storage;
+type AreaName = keyof Pick<
+  ChromeStorage,
+  'sync' | 'local' | 'managed' | 'session'
+>;
+type StorageChangedEvent = ChromeStorage['onChanged'];
+
+function useChromeStorageState<T>(
+  key: string,
+  initialState: T | (() => T),
+  areaName: AreaName = 'sync'
+) {
+  const [value, setValue] = useState(initialState);
+
+  useEffect(() => {
+    const handleStorageChanged: Parameters<
+      StorageChangedEvent['addListener']
+    >[0] = (changes, currentAreaName) => {
+      for (const [currentKey, { newValue }] of Object.entries(changes)) {
+        if (currentKey === key && currentAreaName === areaName) {
+          setValue(newValue);
+        }
+      }
+    };
+
+    chrome.storage.onChanged.addListener(handleStorageChanged);
+    return () => {
+      chrome.storage.onChanged.removeListener(handleStorageChanged);
+    };
+  }, [key]);
+
+  useEffect(() => {
+    chrome.storage[areaName].get(key, (items) => {
+      setValue(items[key]);
+    });
+  }, [areaName, key]);
+
+  const setChromeStorage = useCallback(
+    (value: T) => {
+      chrome.storage[areaName].set({
+        key: value,
+      });
+    },
+    [areaName, key]
+  );
+
+  return [value, setChromeStorage] as const;
+}
+
+export default useChromeStorageState;
