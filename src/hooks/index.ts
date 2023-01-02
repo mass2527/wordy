9cdb3b637560f8342cfe45f482419518a6e7db40
Dongho Kim <dongre2527@gmail.com>
2023-01-02 17:36:25 +0900

    feat: usePersistentCallback hook 제거

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,5 +1,4 @@
 export * from './useChromeStorageState';
 export * from './usePrevious';
 export * from './useFreshRef';
-export * from './usePersistentCallback';
 export * from './useEventListener';
