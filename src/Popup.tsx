ecdcf84f75ad9a145f075d7c296c3a1856efc3dd
Dongho Kim <dongre2527@gmail.com>
2023-01-12 17:32:20 +0900

    feat: popup 불필요한 내용 제거

--- src/Popup.tsx
+++ src/Popup.tsx
@@ -10,20 +10,6 @@ function Popup() {
 
   return (
     <Box css={{ width: '22rem' }}>
-      <Flex
-        as='header'
-        gap={8}
-        align='center'
-        css={{ p: '$16', borderBottom: '1px solid $neutral200' }}
-      >
-        <img src='icon16.png' alt='wordy logo' width={24} height={24} />
-        <Text as='h1'>
-          wordy{' '}
-          <Text color='primary' css={{ fontWeight: '$700' }}>
-            for Chrome
-          </Text>
-        </Text>
-      </Flex>
       <Flex as='main' direction='column' gap={8} css={{ p: '$16' }}>
         <Text as='h2' css={{ fontWeight: '$700' }}>
           설정
